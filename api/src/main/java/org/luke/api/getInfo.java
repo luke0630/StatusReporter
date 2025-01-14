b8117d15f7ef9735aa7f7a98fa8ae231933d63b5
luke0630 <ryuto-0915@outlook.jp>
2025-01-14 17:04:43 +0900

    update: getInfo

--- api/src/main/java/org/luke/api/getInfo.java
+++ api/src/main/java/org/luke/api/getInfo.java
@@ -1,15 +1,19 @@
 package org.luke.api;
 
+import com.google.gson.Gson;
+import lombok.experimental.UtilityClass;
 import org.json.JSONObject;
 import org.luke.statusReporter.StatusReporter;
+import org.luke.statusreporter.DynamicServerData;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@UtilityClass
 public class getInfo {
-    public static String get() {
+    public JSONObject getStatusJSON() {
         try(HttpClient client = HttpClient.newHttpClient()) {
             // HttpClientの作成;
             // HTTPリクエストの作成
@@ -25,15 +29,25 @@ public class getInfo {
             if (response.statusCode() == 200) {
                 // レスポンスボディをJSONとしてパース
                 String responseBody = response.body();
-                JSONObject json = new JSONObject(responseBody);
 
-                return json.toString();
+                return new JSONObject(responseBody);
             } else {
                 System.out.println("HTTP Error: " + response.statusCode());
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
         return null;
+    }
+    public DynamicServerData getStatusByServerData(String serverName) {
+
+         JSONObject status = getStatusJSON();
+         if(status != null) {
+            status.getJSONObject(serverName);
+
+            Gson gson = new Gson();
+            return gson.fromJson(status.toString() ,DynamicServerData.class);
+         }
+         return null;
     }
 }
