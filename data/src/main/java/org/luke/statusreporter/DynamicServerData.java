f1fc7ebce4ed810b60c96ca56345449bd9ef3f65
luke0630 <ryuto-0915@outlook.jp>
2025-01-14 17:04:23 +0900

    move: DynamicServerData

---
+++ data/src/main/java/org/luke/statusreporter/DynamicServerData.java
@@ -0,0 +1,32 @@
+package org.luke.statusreporter;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class DynamicServerData {
+    private String serverName = "";
+    private List<PluginInfo> plugins;
+    private List<PlayerData> players;
+
+    @Setter
+    @Getter
+    public static class PluginInfo {
+        private String pluginName = "";
+        private String mainClass;
+        private String description;
+        private String version;
+        private List<String> authors;
+        private String website;
+    }
+
+    @Setter
+    @Getter
+    public static class PlayerData {
+        private String uuid;
+        private String name;
+    }
+}
