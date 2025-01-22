279a57c8f34bc248bb27d92910cd4114f2a1aebd
luke0630 <ryuto-0915@outlook.jp>
2025-01-23 04:34:45 +0900

    update rm: websocketの再試行関数

--- src/main/java/org/luke/statusReporter/WebSocket/WebSocketClient.java
+++ src/main/java/org/luke/statusReporter/WebSocket/WebSocketClient.java
@@ -3,7 +3,6 @@ package org.luke.statusReporter.WebSocket;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitTask;
 import org.java_websocket.handshake.ServerHandshake;
 import org.json.JSONObject;
 import org.luke.statusReporter.Data.ConfigData;
@@ -30,11 +29,6 @@ public class WebSocketClient extends org.java_websocket.client.WebSocketClient {
 
     @Override
     public void onOpen(ServerHandshake serverHandshake) {
-        // 接続成功時にスケジューラーを停止
-        if (scheduler != null) {
-            scheduler.cancel();
-            scheduler = null; // スケジューラーをクリア
-        }
         System.out.println("サーバーに接続しました");
 
         Register();
@@ -88,27 +82,13 @@ public class WebSocketClient extends org.java_websocket.client.WebSocketClient {
     public void onClose(int code, String reason, boolean remote) {
         System.out.println("Closed connection: " + reason);
 
-        attemptReconnect();
+        Sender.Register();
     }
 
     @Override
     public void onError(Exception ex) {
 
     }
-
-    private static BukkitTask scheduler = null;
-
-    // 再接続を試みるメソッド
-    private static void attemptReconnect() {
-        if(scheduler != null) return;
-        scheduler = Bukkit.getScheduler().runTaskLaterAsynchronously(StatusReporter.getInstance(), () -> {
-            System.out.println("接続を再試行します");
-            StatusReporter.Websocket_ConnectToServer();
-            scheduler.cancel();
-            scheduler = null;
-        }, (long) (reconnectDuration * 20L));
-    }
-
     public void sendMessageToServer(MessageType type, String message) {
         if (isOpen()) {
             JSONObject jsonObject = new JSONObject();
