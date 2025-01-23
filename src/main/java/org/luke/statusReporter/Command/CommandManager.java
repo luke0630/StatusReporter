a27686499912b56dc9bbb0cf09bdab1b304531d9
luke0630 <ryuto-0915@outlook.jp>
2025-01-23 13:41:59 +0900

    update: switch文に置換、reconnectコマンドを削除

--- src/main/java/org/luke/statusReporter/Command/CommandManager.java
+++ src/main/java/org/luke/statusReporter/Command/CommandManager.java
@@ -3,36 +3,31 @@ package org.luke.statusReporter.Command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.luke.statusReporter.API.getInfo;
 import org.luke.statusReporter.Data.DynamicServerData;
-import org.luke.statusReporter.Sender;
 
-import java.util.List;
 import java.util.Objects;
-
-import static org.luke.takoyakiLibrary.TakoUtility.toColor;
 
 public class CommandManager implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, String[] strings) {
-        if(strings[0].equals("reconnect")) {
-            if(commandSender instanceof Player player) {
-                player.sendMessage(toColor("&c接続を再試行しています。 詳細はコンソールを確認してください。"));
+        if(strings.length <= 0) return true;
+        switch(strings[0]) {
+            case "list" -> {
+                getInfo.getStatusList().thenAccept(dynamicServerData -> {
+                    for(DynamicServerData serverData : dynamicServerData) {
+                        commandSender.sendMessage(serverData.getServerName());
+                    }
+                });
             }
-            Sender.Register();
-        }
-        if(strings[0].equals("list")) {
-            List<DynamicServerData> list = getInfo.getStatusList();
-            for(DynamicServerData serverData : list) {
-                commandSender.sendMessage(serverData.getServerName());
+
+            case "status" -> {
+                getInfo.getStatusJSON().thenAccept(jsonResponse -> {
+                    commandSender.sendMessage(Objects.requireNonNullElse(jsonResponse, "Failed to fetch server status."));
+                });
             }
-        } else {
-            getInfo.getStatusJSON().thenApply(jsonResponse -> {
-                commandSender.sendMessage(Objects.requireNonNullElse(jsonResponse, "Failed to fetch server status."));
-                return true;
-            });
+            default -> commandSender.sendMessage("そのコマンドは存在しません");
         }
         return true;
     }
